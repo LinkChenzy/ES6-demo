9141e53f529718fe28bf11e067014bce8d39732d
LinkChenzy <chenzhiyuan0910@163.com>
2018-05-16 23:15:35 +0800

    parameter proxy scope

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import './source/const';
+// import './source/const';
+// import './source/arrow-function';
+// import './source/parmeter';
+// import './source/proxy';
+import './source/scope';
+
 
 
 // let log = (item) => {
--- source/const.js
+++ source/const.js
@@ -3,15 +3,15 @@ const PI = 3.1415926;
 
 // ES5 声明常量的语法
 Object.defineProperty(typeof global === "object" ? global : window, "PI2", {
-    value:        3.1415926,
-    enumerable:   true,
-    writable:     false,
-    configurable: false
+	value: 3.1415926,
+	enumerable: true,
+	writable: false,
+	configurable: false
 });
 
 // 执行运行结果：
 
-console.table([PI, window.PI2])
+console.log([PI, window.PI2])
 
 // 修改常量
 // PI = 4;
